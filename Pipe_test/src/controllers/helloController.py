9bca7a35c0a062f43d90f7782d5d9ca9d8c5a3b2
Andrey <Draizerok@gmail.com>
2021-06-17 23:07:41 +0300

    Добавлены шаблоны HTML с таблицами. Шаблоны модифицируются на месте во время вызова шаблонизатора. По итогам уже есть две таблички MVP близко.

--- Pipe_test/src/controllers/helloController.py
+++ Pipe_test/src/controllers/helloController.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template
 from Pipe_test.src.gateways.MyFirstMongo import getLinks , getLinksInDict
 from Pipe_test.src.services.settingsParser import templateFolder
+from Pipe_test.src.services.service import uniqueFileNameList
 pipe_name = '1288619'
 
-app = Flask(__name__, template_folder=templateFolder)
+app = Flask(__name__, template_folder = 'c:/code/Pipes_checker_v1/Pipe_test/templates')
 
 @app.route('/hello')
 def hello_world():
     return 'helloworld'
 
+@app.route('/files')
+def getAllRecords():
+    """Вернет шаблон со всеми записями"""
+    allRecordsList = []
+    for name in uniqueFileNameList:
+        allRecordsList.append(getLinksInDict(name))
+    return render_template('TemplateForManyPipes.html', allRecords = allRecordsList)
 
-@app.route('/names/%s' % pipe_name)
+
+@app.route('/files/%s' % pipe_name)
 def getRecordByNamer():
-    return render_template('Template.html',**getLinksInDict(pipe_name))
+    """Вернет шаблон с одной записью pipe_name"""
+    return render_template('TemplateForOnePipe.html',**getLinksInDict(pipe_name))
